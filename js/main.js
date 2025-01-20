241b3b1303e7e3fb3490f706e5cf27337c12c49d
root <root@localhost>
2025-01-20 09:00:34 +0800

    auto publish 1737334833545

---
+++ js/main.js
@@ -0,0 +1,28 @@
+09164713768
+johngaryorio21@gmail.com 
+luksuhin, alfonso cavite
+johnorio.com
+
+
+
+Experiences 
+
+KAPE SINAUNA 
+CASHIER/ BARISTA: 2023-2024
+EWAYS RESTAURANT: 2022-2023
+
+Education 
+
+Elementary - Tarlac Elementary school 
+highschool - Rogationist College
+College- Southern Luzon College, Philippines 
+
+achievements 
+ Gll-G12 consistent with Honors 
+ best in photo journalism 
+ 1st place in Poster making contest
+ food and drug awareness
+conduct awardee 
+Class Vice President 2023-2024
+
+
